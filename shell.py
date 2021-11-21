3f9318aaf7704a9d6a84d4827ceb0f2b78f7c3ac
kornellewy <40360922+kornellewy@users.noreply.github.com>
2021-11-21 14:37:17 +0100

    ep1 end

--- shell.py
+++ shell.py
@@ -1,3 +1,14 @@
 """
 https://www.youtube.com/watch?v=Eythq9848Fg&list=PLZQftyCk7_SdoVexSmwy_tBgs7P0b97yD
 """
+
+import basic
+
+while True:
+    text = input("basic > ")
+    result, error = basic.run("<stdin>", text)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
